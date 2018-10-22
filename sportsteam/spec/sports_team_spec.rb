c413873befa78c8987cdfb278e5c5374569adeb3
Catriona Ford <catriona.b.ford@gmail.com>
2018-10-22 20:29:18 +0100

    initial commit

--- sportsteam/spec/sports_team_spec.rb
+++ sportsteam/spec/sports_team_spec.rb
@@ -6,27 +6,39 @@ class TestSportsTeam < MiniTest::Test
 
   def test_make_sportsteam
     team = SportsTeam.new("Team 1", ["Steven","Cat"], "Megabus")
-    assert_equal("Team 1", team.get_team_name)
+    # asert_equal("Team 1", team.get_team_name)
+    assert_equal("Team 1", team.name)
   end
 
   def test_get_team_name
     team = SportsTeam.new("Team 1", ["Steven","Cat"], "Megabus")
-    assert_equal("Team 1", team.get_team_name)
+    # assert_equal("Team 1", team.get_team_name)
+    assert_equal("Team 1", team.name)
   end
 
   def test_get_players
     team = SportsTeam.new("Team 1", ["Steven","Cat"], "Megabus")
-    assert_equal(["Steven","Cat"], team.get_players)
+    # assert_equal(["Steven","Cat"], team.get_players)
+    assert_equal(["Steven","Cat"], team.players)
   end
 
   def test_get_coach
     team = SportsTeam.new("Team 1", ["Steven","Cat"], "Megabus")
-    assert_equal("Megabus", team.get_coach)
+    # assert_equal("Megabus", team.get_coach)
+    assert_equal("Megabus", team.coach)
   end
 
   def test_set_coach_name
     team = SportsTeam.new("Team 1", ["Steven","Cat"], "Megabus")
-    team.set_coach_name("National Express")
-    assert_equal("National Express", team.get_coach)
+    # team.set_coach_name("National Express")
+    team.coach = "National Express"
+    # assert_equal("National Express", team.get_coach)
+    assert_equal("National Express", team.coach)
+  end
+
+  def test_add_new_players
+    team = SportsTeam.new("Team 1", ["Steven","Cat"], "Megabus")
+    team.add_new_players("Jenny")
+    assert_equal(["Steven","Cat", "Jenny"], team.players)
   end
 end
--- sportsteam/sports_team.rb
+++ sportsteam/sports_team.rb
@@ -1,24 +1,32 @@
 class SportsTeam
+
+  attr_reader :name, :players
+  attr_accessor :coach
+
   def initialize(name, players, coach)
     @name = name
     @players = players
     @coach = coach
   end
 
-  def get_team_name
-    return @name
-  end
+  # def get_team_name
+  #   return @name
+  # end
+  #
+  # def get_players
+  #   return @players
+  # end
+  #
+  # def get_coach
+  #   @coach
+  # end
+  #
+  # def set_coach_name(new_coach_name)
+  #   @coach = new_coach_name
+  # end
 
-  def get_players
-    return @players
-  end
-
-  def get_coach
-    @coach
-  end
-
-  def set_coach_name(new_coach_name)
-    @coach = new_coach_name
+  def add_new_players(new_players_array)
+    @players<<new_players_array
   end
 
 end
